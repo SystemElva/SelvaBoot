b803ff0e16ee19978475bad9aa7e8973659b873e
Tito <slashdreama@gmail.com>
2025-07-12 01:31:57 +0000

    fix(bootloader): Correct bash scripting
    - correct the bash script for creating boot image (srh-sh/build.sh file)

--- Loader/i686-bios/src-sh/build.sh
+++ Loader/i686-bios/src-sh/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(dirname $0)/..
+cd "$(dirname "$0")"/.. || exit
 I686_PATH=$(pwd)
 
 BOOTSECTOR_OBJECTS="$I686_PATH/.build/modules/bootsector"
@@ -19,33 +19,33 @@ COLOR_RESET='\033[0m'
 
 if [[ ! -f $BOOTFS_PARTITION/fat12.img ]];
 then
-    printf "${COLOR_YELLOW}INFO: Didn't find an boot-fs. Creating one!${COLOR_RESET}\n"
-    $I686_PATH/src-sh/makefs.sh
+    printf "%sINFO: Didn't find an boot-fs. Creating one!%s\n" "$COLOR_YELLOW" "$COLOR_RESET"
+    "$I686_PATH"/src-sh/makefs.sh
 fi
 
 # Create the objects folder, in case it hasn't been created before
-mkdir -p $BOOTSECTOR_OBJECTS
-mkdir -p $CODE_PARTITION_OBJECTS
-mkdir -p $OUTPUT_DIRECTORY
+mkdir -p "$BOOTSECTOR_OBJECTS"
+mkdir -p "$CODE_PARTITION_OBJECTS"
+mkdir -p "$OUTPUT_DIRECTORY"
 
 # Make sure that no old object-files are in the objects-folder
-rm -f $BOOTSECTOR_OBJECTS/*.o
-rm -f $CODE_PARTITION_OBJECTS/*.o
+rm -f "$BOOTSECTOR_OBJECTS"/*.o
+rm -f "$CODE_PARTITION_OBJECTS"/*.o
 
-nasm -o $BOOTSECTOR_OBJECTS/object.bin \
-    $BOOTSECTOR_SOURCES/bootsector.asm \
-    -i $BOOTSECTOR_SOURCES
+nasm -o "$BOOTSECTOR_OBJECTS"/object.bin \
+    "$BOOTSECTOR_SOURCES"/bootsector.asm \
+    -i "$BOOTSECTOR_SOURCES"
 
-nasm -o $CODE_PARTITION_OBJECTS/object.bin \
-    $CODE_PARTITION_SOURCES/main.asm \
+nasm -o "$CODE_PARTITION_OBJECTS"/object.bin \
+    "$CODE_PARTITION_SOURCES"/main.asm \
     -i "$CODE_PARTITION_SOURCES"
 
-OUTPUT_FILE=$OUTPUT_DIRECTORY/elvaboot-$EXECUTION_TIME.img
+OUTPUT_FILE="$OUTPUT_DIRECTORY"/elvaboot-$EXECUTION_TIME.img
 
 HD_DISKETTE_BYTES=$((2880*512)) # 2880 sectors of 512 bytes each
-truncate $OUTPUT_FILE --size=$HD_DISKETTE_BYTES
+truncate "$OUTPUT_FILE" --size=$HD_DISKETTE_BYTES
 
-dd status=none conv=notrunc cbs=512 if=$BOOTSECTOR_OBJECTS/object.bin of=$OUTPUT_FILE
-dd status=none conv=notrunc cbs=512 if=$CODE_PARTITION_OBJECTS/object.bin of=$OUTPUT_FILE seek=1
-dd status=none conv=notrunc cbs=512 if=$BOOTFS_PARTITION/fat12.img of=$OUTPUT_FILE seek=64
+dd status=none conv=notrunc cbs=512 if="$BOOTSECTOR_OBJECTS"/object.bin of="$OUTPUT_FILE"
+dd status=none conv=notrunc cbs=512 if="$CODE_PARTITION_OBJECTS"/object.bin of="$OUTPUT_FILE" seek=1
+dd status=none conv=notrunc cbs=512 if="$BOOTFS_PARTITION"/fat12.img of="$OUTPUT_FILE" seek=64
 
